707153c3c42d6a92fea0ce7c9127a1b916345deb
Son of Jongkyun, Park <100128652+jhp2014@users.noreply.github.com>
2023-11-02 17:13:55 +0900

    feat: MemberRepository 생성
    이메일을 통해 회원 가입 여부 및 회원 정보 조회 가능

---
+++ src/main/java/com/project/bookstudy/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.project.bookstudy.member.repository;
+
+import com.project.bookstudy.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
