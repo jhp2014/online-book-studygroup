e81e091b0042bb156cef900240c1ff21f6bbec07
Son of Jongkyun, Park <100128652+jhp2014@users.noreply.github.com>
2023-11-02 17:03:11 +0900

    feat: Member Entity에서 사용할 BaseEntity 설정
    등록 시간 및 수정 시간 자동 으로 기록 되도록 한다

--- src/test/java/com/project/bookstudy/BookStudyApplicationTests.java
+++ src/test/java/com/project/bookstudy/BookStudyApplicationTests.java
@@ -2,8 +2,10 @@ package com.project.bookstudy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
+@EnableJpaAuditing
 class BookStudyApplicationTests {
 
 	@Test
